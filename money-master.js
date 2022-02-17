dfa96620a35f138e5373085a29df59533e2eb951
RajuSultan <rajusultan12@gmail.com>
2022-02-17 20:59:13 +0600

    forth commit

--- money-master.js
+++ money-master.js
@@ -1,3 +1,5 @@
+
+//--------food ,rent, clothes and income input function------//
 function expansesInput(inputId) {
 
     const input = document.getElementById(inputId);
@@ -10,18 +12,22 @@ function expansesInput(inputId) {
     }
 
 }
+//------------calculation button addEventListener--------//
 document.getElementById("cal-expanses").addEventListener("click", function () {
     const income = expansesInput("income-input");
     const food = expansesInput("food-input");
     const rent = expansesInput("rent-input");
     const clothes = expansesInput("clothes-input");
+    //-------------total Expanses calculation---------//
     const totalExpanses = food + rent + clothes;
     const expanses = document.getElementById("total-expanses");
 
+    //-------------Nan error check----------//
     if (isNaN(totalExpanses)) {
         console.log("Enter the Valid Number!");
         expanses.innerText = "00";
     } else {
+        //--------over expanses alert--------//
         if (totalExpanses > income) {
             alert("Your Expanses is over your income!");
             expanses.innerText = totalExpanses;
@@ -30,38 +36,49 @@ document.getElementById("cal-expanses").addEventListener("click", function () {
         }
     }
 
-    const balanceInner = document.getElementById("balance");
-    const balance = income - totalExpanses;
+    //-----------get balance----------//
 
+    const balanceInner = document.getElementById("balance");
+    //----------balance calculation----------//
+    const balance = income - totalExpanses;
+    //-------------Nan error check----------//
     if (isNaN(totalExpanses) || isNaN(income)) {
         console.log("Enter the Valid Number!");
         balanceInner.innerText = "00";
     } else {
+        //----------negative error check-----//
         if (balance < 0) {
             alert("Your are in Lose" + balance);
-
         } else {
             balanceInner.innerText = balance;
         }
     }
 
 })
+
+//------------save button addEventListener-----------//
 document.getElementById("save-btn").addEventListener("click", function () {
+    //-----------collect Parcentage----------//
     const saveInputParsentage = document.getElementById("save-input");
     const saveInputParsentageText = saveInputParsentage.value;
     const saveInputParsentageValue = parseInt(saveInputParsentageText);
     const income = expansesInput("income-input");
+    //-----------parcent amount calculation---------//
     const savingAmount = (income * saveInputParsentageValue) / 100;
     const savingAmountInner = document.getElementById("saving-amount");
+    //---------get balance------------//
     const balanceInner = document.getElementById("balance");
     const balance = parseInt(balanceInner.innerText);
+    //----------remainingAmount calculation-------------//
     const remainingAmount = balance - savingAmount;
     const remainingAmountInner = document.getElementById("remaining-amount");
+    //-------------Nan error check----------//
     if (isNaN(saveInputParsentageValue)) {
         alert("Enter the Valid Paesentage Number!");
         savingAmountInner.innerText = "00";
         remainingAmountInner.innerText = "00";
     } else {
+        //----------negative error check-----//
         if (saveInputParsentageValue > 0) {
             savingAmountInner.innerText = savingAmount;
             if (remainingAmount >= 0) {
